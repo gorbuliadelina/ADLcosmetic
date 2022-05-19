2506565ac6377083b70d2fdd5a6f83880d20accd
gorbuliadelina <105843116+gorbuliadelina@users.noreply.github.com>
2022-05-19 09:36:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const navLinks = document.querySelector('#navLinks');
+const navToggleBtn = document.querySelector('#navToggleBtn');
+
+navToggleBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
